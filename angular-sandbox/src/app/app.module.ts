e251668ab6d028dfcb99572a18b8378fd933db0f
abdumamdouh <eabdogfx@gmail.com>
2022-08-01 23:42:56 +0200

    add comments to explain some angular APIs

--- angular-sandbox/src/app/app.module.ts
+++ angular-sandbox/src/app/app.module.ts
@@ -5,12 +5,17 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
+    //every component you will made will be imported to this file then added here in this array
+    //this will allow you to use the components everywhere without importing them like React
     AppComponent
   ],
   imports: [
+    //every module you will made will be imported to this file then added here in this array
     BrowserModule
   ],
+    //every servvicd you will made will be imported to this file then added here in this array
   providers: [],
+  //the component you want to bootstrap which is the root app component
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- angular-sandbox/src/main.ts
+++ angular-sandbox/src/main.ts
@@ -4,9 +4,11 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+//the environment object is used to know the current environment the angular app is running on
 if (environment.production) {
   enableProdMode();
 }
 
+//bootstrapping the AppModule
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
