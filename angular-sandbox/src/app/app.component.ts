da3bafbb9d868431657aad48108b89e881dc1d35
abdumamdouh <eabdogfx@gmail.com>
2022-08-05 15:07:54 +0200

    add notes to the main angular APIs

--- angular-sandbox/src/app/app.component.ts
+++ angular-sandbox/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  // you can add direct html/template in a property call 'template' or point to a file using templateUrl
   templateUrl: './app.component.html',
+  // you can add direct css/style in a property call 'template' or point to a file using styleUrl
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+//to add ngOnInit life cycle hook you need to import OnInit class then make you component/class implements the OnInit
+export class AppComponent implements OnInit {
+  // class property, think about it like state in React
+  // you can access the class properties and methods in the template using string interpolation
   title = 'angular-sandbox';
+
+  // the constructor is used to inject dependency into the component
+  constructor() {}
+
+  // think about it like componentDidMount
+  ngOnInit() {}
+
+  /*
+  The constructor() should only be used to initialize class members but shouldn't do actual "work". So we should use constructor() to setup Dependency Injection, Initialization of class fields etc. ngOnInit() is a better place to write "actual work code" that we need to execute as soon as the class is instantiated.
+  */
+
+
+
 }
--- angular-sandbox/src/app/app.module.ts
+++ angular-sandbox/src/app/app.module.ts
@@ -3,17 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+//most of the time you won't add to these arrays, if you use the CLI. it will be added automatically
 @NgModule({
   declarations: [
-    //every component you will made will be imported to this file then added here in this array
+    //every component you will make will be imported to this file then added here in this array
     //this will allow you to use the components everywhere without importing them like React
     AppComponent
   ],
   imports: [
-    //every module you will made will be imported to this file then added here in this array
+    //every module you will make will be imported to this file then added here in this array
     BrowserModule
   ],
-    //every servvicd you will made will be imported to this file then added here in this array
+    //every service you will make will be imported to this file then added here in this array
   providers: [],
   //the component you want to bootstrap which is the root app component
   bootstrap: [AppComponent]
