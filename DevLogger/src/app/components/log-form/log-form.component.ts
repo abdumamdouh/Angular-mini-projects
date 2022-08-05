b07459a4d3d4b1cef2f44dc69c707aa997713046
abdumamdouh <eabdogfx@gmail.com>
2022-08-05 22:54:37 +0200

    subscribe for the data stream in the log service

--- DevLogger/src/app/components/log-form/log-form.component.ts
+++ DevLogger/src/app/components/log-form/log-form.component.ts
@@ -1,16 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Log } from 'src/app/models/Log';
 
+import { LogService } from 'src/app/services/log.service';
 @Component({
   selector: 'app-log-form',
   templateUrl: './log-form.component.html',
-  styleUrls: ['./log-form.component.css']
+  styleUrls: ['./log-form.component.css'],
 })
 export class LogFormComponent implements OnInit {
-  text: string = '';
+  // text: string = '';
+  log: Log = {
+    id: 0,
+    text: '',
+    date: '',
+  };
 
-  constructor() { }
+  constructor(private logService: LogService) {}
 
   ngOnInit(): void {
+    // subscribe to the behavior object data stream to get the clicked/selected log
+    this.logService.selectedLog.subscribe((log) => (this.log = log));
   }
-
 }
