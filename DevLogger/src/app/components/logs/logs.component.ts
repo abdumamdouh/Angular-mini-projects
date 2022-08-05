dd4eb6911775b03a1c70b833824060ccf003dce9
abdumamdouh <eabdogfx@gmail.com>
2022-08-05 22:54:15 +0200

    click event to emit the current log to the service

--- DevLogger/src/app/components/logs/logs.component.ts
+++ DevLogger/src/app/components/logs/logs.component.ts
@@ -11,9 +11,19 @@ import { LogService } from 'src/app/services/log.service';
 export class LogsComponent implements OnInit {
   logs: Log[] = [];
 
+  // inject the log service to the constructor so we can use the methods inside the service
+  // dependency injection
+  // when you declare the visibility at the constructor brackets in typescript
+  // => means that you has made a property then assign to it a value and now it's a property in the component
   constructor(private logService: LogService) {}
 
   ngOnInit(): void {
     this.logs = this.logService.getLogs();
+  }
+
+  // emit the clicked log to the behavior subject object so we can subscribe for it at the log from
+  handleLogClick(log: Log) {
+    // console.log(log);
+    this.logService.setFormLog(log);
   }
 }
