bac363ce227aa06412fc560e820ea3618348d18e
abdumamdouh <eabdogfx@gmail.com>
2022-08-06 18:03:32 +0200

    implement localstorage and sync the CRUD operation with it

--- DevLogger/src/app/services/log.service.ts
+++ DevLogger/src/app/services/log.service.ts
@@ -23,43 +23,42 @@ export class LogService {
   constructor() {
     //you can use a static data here at first
     // then you can make ajax calls here in the service in methods that can be used in the components
-    this.logs = [
-      {
-        id: 1,
-        text: 'Marwan Mousaa',
-        date: new Date('8/4/2022 06:40:15'),
-      },
-      {
-        id: 2,
-        text: 'Wegz',
-        date: new Date('8/3/2022 02:04:00'),
-      },
-      {
-        id: 3,
-        text: 'Marwan Pablo',
-        date: new Date('8/2/2022 12:20:11'),
-      },
-      {
-        id: 4,
-        text: 'Moussa Sam',
-        date: new Date('8/1/2022 01:50:20'),
-      },
-      {
-        id: 5,
-        text: 'Abyusif',
-        date: new Date('8/5/2022 12:54:23'),
-      },
-    ];
+
+    // check if there is a logs in the local storage
+    if (!localStorage.getItem('logs')) {
+      const initialLogs = [
+        {
+          id: 1,
+          text: 'Marwan Mousaa',
+          date: new Date('8/4/2022 06:40:15'),
+        },
+        {
+          id: 2,
+          text: 'Wegz',
+          date: new Date('8/3/2022 02:04:00'),
+        },
+        {
+          id: 3,
+          text: 'Marwan Pablo',
+          date: new Date('8/2/2022 12:20:11'),
+        },
+        {
+          id: 4,
+          text: 'Moussa Sam',
+          date: new Date('8/1/2022 01:50:20'),
+        },
+        {
+          id: 5,
+          text: 'Abyusif',
+          date: new Date('8/5/2022 12:54:23'),
+        },
+      ];
+      localStorage.setItem('logs', JSON.stringify(initialLogs));
+    } else this.logs = JSON.parse(localStorage.getItem('logs') as string);
   }
 
   // you can't use ngOnInit in a service, you can use it only on components, use the constructor method instead
   // ngOnInit(): void {}
-
-  // make it to return the logs as observables to be asynchronous
-  getLogs(): Observable<Log[]> {
-    // of operator is a pure method that transform information into the observables stream
-    return of(this.logs);
-  }
 
   // will use this method to set form log to the data source so every subscriber to this BehaviorSubject object gets the new data
   setFormLog(log: Log) {
@@ -69,31 +68,35 @@ export class LogService {
     // console.log(log);
   }
 
+  syncLocalStorage() {
+    localStorage.setItem('logs', JSON.stringify(this.logs));
+  }
+
+  // CRUD operations
+
   // add log
   addLog(log: Log) {
     log.id = this.logs.length + 1;
     // console.log('from service', log);
+    // update the UI
     this.logs.unshift(log);
+    // save the changes to local storage
+    this.syncLocalStorage();
     // you can't reassign the reference type with a new value, edit in place, in React you clone but in Angular edit in place
     // this.logs = [log, ...this.logs];
     // console.log(this.logs);
   }
 
-  //delete log
-  deleteLog(log: Log) {
-    console.log('from service', log);
-    const { id } = log;
-    // this won't work
-    // this.logs = this.logs.filter((log) => log.id !== id);
-    this.logs.forEach((log, index) => {
-      if (log.id === id) this.logs.splice(index, 1);
-    });
+  // get logs
+  // make it to return the logs as observables to be asynchronous
+  getLogs(): Observable<Log[]> {
+    // of operator is a pure method that transform information into the observables stream
+    return of(this.logs);
   }
 
   //delete log
   updateLog(log: Log) {
-    console.log(log);
-
+    // console.log(log);
     const { id } = log;
     this.logs.forEach((l, index) => {
       if (l.id === id) {
@@ -102,6 +105,22 @@ export class LogService {
         this.logs[index].date = new Date();
       }
     });
-    console.log(this.logs);
+
+    // console.log(this.logs);
+    // save changes to local storage
+    this.syncLocalStorage();
+  }
+
+  //delete log
+  deleteLog(log: Log) {
+    // console.log('from service', log);
+    const { id } = log;
+    // this won't work
+    // this.logs = this.logs.filter((log) => log.id !== id);
+    this.logs.forEach((log, index) => {
+      if (log.id === id) this.logs.splice(index, 1);
+    });
+    // save changes to local storage
+    this.syncLocalStorage();
   }
 }
